aa23ed53f948047bf5f7dede477c71b535325d43
Yin Li <liyin@xiaomi.com>
2020-05-12 22:06:07 +0100

    turn axis to screen axis

--- js/scripts/graph.js
+++ js/scripts/graph.js
@@ -7,9 +7,11 @@ var params = { width: 800, height: 600, type:Two.Types.svg };
 var two = new Two(params).appendTo(elem);
 
 var scale = 1
+var min_x = 1000000, max_x = 0, min_y = 1000000, max_y = 0;
 
 function drawConnectedLine(p1, p2) {
-    var line = two.makeLine(p1["pos"][0], p1["pos"][1], p2["pos"][0], p2["pos"][1]);
+    var line = two.makeLine(p1["pos"][0] - min_x, params.height - (p1["pos"][1] - min_y),
+        p2["pos"][0] - min_x, params.height - (p2["pos"][1] - min_y));
     line.linewidth = 3;
     line.stroke = "rgba(255, 0, 0, 0.5)";
     console.log(p1, p2);
@@ -32,25 +34,12 @@ function drawPolygon(polygon) {
 }
 
 function normalize(graph) {
-    var min_x = 1000000, max_x = 0, min_y = 1000000, max_y = 0;
     for (var plg of graph["polygon"]) {
         for (var p of plg["point"]) {
             min_x = Math.min(min_x, p["pos"][0]);
             max_x = Math.max(max_x, p["pos"][0]);
             min_y = Math.min(min_y, p["pos"][1]);
             max_y = Math.max(max_y, p["pos"][1]);
-        }
-    }
-    var cx = (min_x + max_x) * 0.5;
-    var cy = (min_y + max_y) * 0.5;
-
-    console.log(cx);
-    console.log(cy);
-
-    for (var plg of graph["polygon"]) {
-        for (var p of plg["point"]) {
-            p["pos"][0] = p["pos"][0] - min_x;
-            p["pos"][1] = p["pos"][1] - min_y;
         }
     }
 }
--- tools/configure.sh
+++ tools/configure.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+
+open -a /Applications/Google\ Chrome.app --args --disable-web-security --user-data-dir --allow-file-access-from-files
